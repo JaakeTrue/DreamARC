ce0268f8000d1f1102823f98564f83434954950c
Jake Ryu <ryujihoon@fuller.edu>
2026-02-15 21:06:28 -0800

    Fix SPA redirects, VITE_API_BASE, and FastAPI CORS

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,10 @@
-﻿import { BrowserRouter } from 'react-router-dom';
-// Compatibility entry for Vite/Cloudflare HTML that expects /src/main.jsx
-// Do NOT remove: mentor-tutors.org currently references /src/main.jsx
-import "./index.jsx";
+﻿import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-
-
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
